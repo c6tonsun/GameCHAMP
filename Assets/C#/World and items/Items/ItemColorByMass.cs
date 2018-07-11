a3a6ebbb979a9980a88a5352c8dce1c8595ab46b
c6tonsun <toni.sundell@cs.tamk.fi>
2018-07-11 17:15:15 +0300

    New item color swapping and default color by weight.

---
+++ Assets/C#/World and items/Items/ItemColorByMass.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ItemColorByMass : MonoBehaviour
+{
+    private GameManager _gameManager;
+    private MeshRenderer _mr;
+
+    public int materialIndex;
+    private Color _defaultColor;
+
+    private void Start()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
+        _mr = GetComponent<MeshRenderer>();
+
+        #region defaultColor
+
+        float mass = GetComponent<Rigidbody>().mass;
+
+        int shorter;
+        if (_gameManager.itemMassTreshholds.Length > _gameManager.itemColorsByMass.Length)
+            shorter = _gameManager.itemColorsByMass.Length;
+        else
+            shorter = _gameManager.itemMassTreshholds.Length;
+
+        for (int i = 0; i < shorter; i++)
+        {
+            if (mass < _gameManager.itemMassTreshholds[i])
+            {
+                _defaultColor = _gameManager.itemColorsByMass[i];
+                mass = -1;
+                break;
+            }
+        }
+
+        if (mass > 0)
+            _defaultColor = _gameManager.itemModeColors[0];
+
+        #endregion
+
+        _mr.materials[materialIndex].color = _defaultColor;
+    }
+}
