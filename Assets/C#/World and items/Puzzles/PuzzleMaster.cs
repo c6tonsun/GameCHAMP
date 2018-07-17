8f0a751e769b7a5d63bfb7a707dfccb0d1a5ec73
c6tonsun <toni.sundell@cs.tamk.fi>
2018-07-17 16:58:57 +0300

    Stage error :)

---
+++ Assets/C#/World and items/Puzzles/PuzzleMaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PuzzleMaster : MonoBehaviour
+{
+    public IPuzzlePiece[] pieces;
+    public SlideDoor door;
+    private bool openDoor;
+
+    private void Start()
+    {
+        foreach (IPuzzlePiece piece in pieces)
+            piece.PuzzleMaster = this;
+    }
+
+    public void CheckPuzzlePieces()
+    {
+        foreach (IPuzzlePiece piece in pieces)
+        {
+            if (!piece.IsSendingSignal())
+            {
+                openDoor = false;
+                break;
+            }
+        }
+
+        if (openDoor)
+            door.Open();
+        else
+            door.Close();
+    }
+}
